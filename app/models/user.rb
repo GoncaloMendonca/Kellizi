21e8cc9582fb5423977cace54d931dba23593ca1
LouisDucurtil <louis.ducurtil@gmail.com>
2022-08-29 16:00:00 +0100

    test commit 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :contracts, dependent: :destroy
+  has_many :coverages, through: :contract
+  has_many :products, through: :contract
+  has_many :companies, through: :product
 end
