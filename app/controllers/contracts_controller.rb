a8fc98562f2350b74160b74b169e3f6dcc25fe19
Goncalo Mendonca <dflomendonca@gmail.com>
2022-08-29 18:00:05 +0100

    Editing Controllers

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,2 +1,45 @@
 class ContractsController < ApplicationController
+  def index
+    @contract = Contract.all
+  end
+
+  def show
+    @contract = contract.find(params[:id])
+  end
+
+  def new
+    @contract = Contract.new
+  end
+
+  def create
+    @contract = Contract.new(contract_params)
+    if @contract.save
+      @name = @contract.name.to_s
+      redirect_to insurance_product_contract_index_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @contract = Contract.find(params[:id])
+    @file = File.all
+    @status = Status.all
+  end
+
+  def update
+    @contract =
+  end
+
+  def destroy
+    @contract = Contract.find(params[:id])
+    @coontract.destroy
+    redirect_to insurance_product_contract_index_path, status: :see_other
+  end
+
+  private
+
+  def contract_params
+    params.require(:contract).permit(:price, :source, :start_at, :ends_at, :status, :created_at, :updated_at)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :insurance, only: [:show] do
+  resources :insurance, only: [:show, :index] do
     resources :product, only: [:index] do
       resources :contract do
         resources :coverage, only: [:show, :index]
